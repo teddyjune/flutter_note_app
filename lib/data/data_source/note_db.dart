87091253ed2cdf9744388ea87cebe4d18c7e83a6
yeonjoo.lee <teddyjune@naver.com>
2022-09-10 18:28:04 +0900

    add: repository 생성

---
+++ lib/data/data_source/note_db.dart
@@ -0,0 +1,46 @@
+import 'package:sqflite/sqflite.dart';
+
+import '../../domain/model/note.dart';
+
+class NoteDb {
+  Database db;
+
+  NoteDb(this.db);
+
+  Future<Note?> getNoteById(int id) async {
+    //SELECT * FROM note WHERE id = 1과 같은 의미
+    final List<Map<String, dynamic>> maps =
+        await db.query('note', where: 'id = ?', whereArgs: [id]);
+    if (maps.isNotEmpty) {
+      return Note.fromJson(maps.first);
+    }
+    return null;
+  }
+
+  Future<List<Note>> getNotes() async {
+    //note 테이블의 모두를 가져오겠다.
+    final maps = await db.query('note');
+    return maps.map((e) => Note.fromJson(e)).toList();
+  }
+
+  Future<void> insertNote(Note note) async {
+    await db.insert('note', note.toJson());
+  }
+
+  Future<void> updateNote(Note note) async {
+    await db.update(
+      'note',
+      note.toJson(),
+      where: 'id = ?',
+      whereArgs: [note.id],
+    );
+  }
+
+  Future<void> deleteNote(Note note) async {
+    await db.delete(
+      'note',
+      where: 'id = ?',
+      whereArgs: [note.id],
+    );
+  }
+}
